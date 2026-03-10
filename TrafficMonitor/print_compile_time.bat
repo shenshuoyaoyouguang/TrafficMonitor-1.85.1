2967617f97b760c84cdd06ea9fb09ebf647f4de5
chujian <765781379@qq.com>
2026-03-11 00:11:49 +0800

    build: stabilize compile time generation

--- TrafficMonitor/print_compile_time.bat
+++ TrafficMonitor/print_compile_time.bat
@@ -1,4 +1,9 @@
-REM 输出当前日期和时间，并保存到compile_time.txt文件中
+@echo off
+REM Write current local date/time into compile_time.txt in a stable format.
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyyy/MM/dd\""') do set DATE_STR=%%i
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"HH:mm:ss\""') do set TIME_STR=%%i
 del /F /Q compile_time.txt
-echo %date:~0,10% >> compile_time.txt
-echo %time:~0,8% >> compile_time.txt
+(
+  echo %DATE_STR%
+  echo %TIME_STR%
+)>> compile_time.txt
